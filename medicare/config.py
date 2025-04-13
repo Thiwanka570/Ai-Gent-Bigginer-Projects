e6d8667d2513959afbeabba2c4f70146b2453f7e
thiwanka570 <jvptmadushan@gmail.com>
2025-04-13 19:08:42 +0530

    Signed-off-by: thiwanka570 <jvptmadushan@gmail.com>

--- medicare/config.py
+++ medicare/config.py
@@ -5,9 +5,9 @@ import os
 load_dotenv()
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://md570:thiwanka%40570MongoDb@cluster0.ys1pg.mongodb.net/url_shortner?retryWrites=true&w=majority&appName=Cluster0")
-    DB_NAME = "medicare"
-    GEMINI_API_KEY="AIzaSyAvbhHecGbjoM9W58nPHhAk1ZEx-SytwqY"
+    MONGO_URI = os.getenv("MONGO_URI", "")
+    DB_NAME = ""
+    GEMINI_API_KEY=""
     @staticmethod
     def get_db():
         client = MongoClient(Config.MONGO_URI)
